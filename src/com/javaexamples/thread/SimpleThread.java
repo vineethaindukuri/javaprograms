e612a726eeb9195c927ed6545cb1e880dd788133
vineethag <vineeetha.indukuri@gmail.com>
2015-09-12 13:46:22 -0700

    Refactored the variable names, and improved the message format.

--- src/com/javaexamples/thread/SimpleThread.java
+++ src/com/javaexamples/thread/SimpleThread.java
@@ -7,26 +7,27 @@ import java.util.Date;
 public class SimpleThread extends Thread {
 
     public void run() {
-        System.out.println("Hello from thread " + Thread.currentThread().getName());
+    	String threadName = Thread.currentThread().getName();
+        System.out.println("Hello from thread " + threadName + " "+  new Date());
         try {
 			Thread.sleep(20000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        System.out.println("Inside thread after sleep"+ (new Date()));
+        System.out.println("Inside thread " + threadName + " after sleep "+ new Date());
     }
 
     public static void main(String args[]) throws InterruptedException {
-    	SimpleThread ht1 = new SimpleThread();
-        ht1.start();
-        SimpleThread ht2 = new SimpleThread();
-        ht2.start();
-        System.out.println("Thread " + Thread.currentThread().getName()+ (new Date()));
-        System.out.println("Inside Main" + (new Date()));
+    	SimpleThread simpleThread1 = new SimpleThread();
+        simpleThread1.start();
+        SimpleThread simpleThread2 = new SimpleThread();
+        simpleThread2.start();
+        System.out.println("Thread " + Thread.currentThread().getName()+ " " + new Date());
+        System.out.println("Inside Main " + (new Date()));
         Thread.sleep(10000);
-        //main thread waits for the ht1 thread
-        ht1.join();
-        System.out.println("in main "+(new Date()));
+        //main thread waits for the simpleThread1 to finish
+        simpleThread1.join();
+        System.out.println("In main "+(new Date()));
     }
 }
